1dbd23ff303a899d2c424e0beb32714df39bcab8
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2021-07-10 10:55:23 +0530

    Create translation.py

---
+++ bot/translation.py
@@ -0,0 +1,25 @@
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<i>Am Just A Advance Auto Filter Bot....😉
+Works only for @HoneyBeeMovies 
+For More Details Click Help Button Below..
+</i>"""    
+    
+    HELP_TEXT = """
+<b><i>Need any Help!?</i></b>
+
+<i>
+-> First Buy me a Beer 🍺 .Then We can talk
+-> Life is very short nanba 😉 Always be Happy
+</i>
+
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> John Durai</code>
+    
+<b>➥ Nickname</b> : <b><i><a href="https://t.me/thejdbot">J.D</a></i></b>
+
+<b>➥ Channel</b> : <b><i><a href="https://t.me/honeybeemovies">Honey Bee Movies</a></i></b>
+
+"""
