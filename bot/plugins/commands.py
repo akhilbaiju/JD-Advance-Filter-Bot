da32e210c617c57dcba1bfbb47cec3acd0d61b4b
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2021-08-24 16:58:56 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -40,12 +40,12 @@ async def start(bot, update):
                     ]
                 )
              )
-            await bot.send_message(
-                chat_id=update.chat.id,
-                text=f"<b><i>🌸🌸 ഈ പൂക്കളം ഒന്നു വോട്ടു ചെയ്ത് സപ്പോർട്ട് ചെയ്യാമോ / Please Vote for this 👇🏻👇🏻 \n \n  https://onam.clusterdev.com/share/96a7c89e7f \n.</b> </i>",
-                parse_mode="html",
-                reply_to_message_id=update.message_id
-            )
+      #      await bot.send_message(
+       #         chat_id=update.chat.id,
+        #        text=f"<b><i>Exrtra Message needed for subscribers can be added here \n.</b> </i>",
+         #       parse_mode="html",
+          #      reply_to_message_id=update.message_id
+           # )
 
         elif file_type == "video":
         
