b3f0cc251294185e46cc7a865856ca5c326b3002
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2021-08-24 17:34:33 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -16,14 +16,14 @@ For More Details Click Help Button Below..
 
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b>    : <code> John Durai</code>
+    ABOUT_TEXT = """<b>➥ Name</b>        : <code> John Durai</code>
     
 <b>➥ Nickname</b> : <b><i><a href="https://t.me/thejdbot">J.D</a></i></b>
 
-<b>➥ Creator</b>  : <b><i><a href="https://t.me/akhilbaiju">Unni</a></i></b>
+<b>➥ Creator</b>      : <b><i><a href="https://t.me/akhilbaiju">Unni</a></i></b>
 
-<b>➥ Language</b> : <code> Python 3</code>
+<b>➥ Language</b>  : <code> Python 3</code>
 
-<b>➥ Library</b> : <b><i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i></b>
+<b>➥ Library</b>        : <b><i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i></b>
 
 """
